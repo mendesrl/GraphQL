42de6a1250e33263dca00aeb09ecb879a55c0313
mendesrl <ribeiro.larissamendes@gmail.com>
2023-03-21 19:37:49 -0400

    12. Configuração do Apollo Server

--- curso-graphql/schema-query/projeto/index.js
+++ curso-graphql/schema-query/projeto/index.js
@@ -1,1 +1,13 @@
-console.log('teste')
+const { ApolloServer, gql } = require('apollo-server');
+const typeDefs = gql``
+const resolvers = {
+
+}
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({url})=>{
+    console.log(`listening ${url}`)
+})
