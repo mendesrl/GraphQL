3e7e2bfb162ad4663bc1028f8361f535fd41fec4
mendesrl <ribeiro.larissamendes@gmail.com>
2023-03-21 20:53:38 -0400

    14. Desafio Hora Atual

--- curso-graphql/schema-query/projeto/index.js
+++ curso-graphql/schema-query/projeto/index.js
@@ -3,13 +3,19 @@ const typeDefs = gql`
     #Pontos de entrada da sua API!
     type Query {
         ola: String
+        horaCerta: String
     }
 `
 const resolvers = {
     Query: {
         ola() {
             return 'Basta retornar uma string'
+        },
+        horaCerta() {
+            const date = new Date().toISOString().slice(0,10);
+            return date
         }
+
     }
 }
 const server = new ApolloServer({
