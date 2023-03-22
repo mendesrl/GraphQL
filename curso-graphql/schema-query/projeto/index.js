4d45005e444cc3e6de3f9a0fa3614003a0b64824
mendesrl <ribeiro.larissamendes@gmail.com>
2023-03-21 21:14:55 -0400

    17. Criando Primeiro Tipo

--- curso-graphql/schema-query/projeto/index.js
+++ curso-graphql/schema-query/projeto/index.js
@@ -2,12 +2,24 @@ const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = gql`
     scalar Date 
 
+    type Usuario {
+        id: ID!
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
     #Pontos de entrada da sua API!
     type Query {
         ola: String
         horaCerta: String
         hora: Date
+        usuarioLogado: Usuario
     }
+
+    
 `
 const resolvers = {
     Query: {
@@ -20,6 +32,16 @@ const resolvers = {
         },
         hora() {
             return new Date
+        },
+        usuarioLogado() {
+            return {
+                id: 1,
+                nome: "Larissa",
+                email: "lalla@gmail.com",
+                idade: 30,
+                salario: 1345.67,
+                vip: true
+            }
         }
 
     }
