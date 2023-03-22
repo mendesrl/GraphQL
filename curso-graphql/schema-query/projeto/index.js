ca888e1c57e0f96cb5fdf72658223362104d7988
mendesrl <ribeiro.larissamendes@gmail.com>
2023-03-21 21:47:37 -0400

    19. Desafio Produto

--- curso-graphql/schema-query/projeto/index.js
+++ curso-graphql/schema-query/projeto/index.js
@@ -11,17 +11,30 @@ const typeDefs = gql`
         vip: Boolean
     }
 
+    type Produto {
+        nome: String!
+        preco: Float!
+        desconto: Float
+        precoComDesconto: Float
+
+    }
+
     #Pontos de entrada da sua API!
     type Query {
         ola: String
         horaCerta: String
         hora: Date
         usuarioLogado: Usuario
+        produtoEmDestaque: Produto
     }
-
     
 `
 const resolvers = {
+    Produto: {
+        precoComDesconto(produto) {
+            return produto.preco - produto.desconto
+        }
+    },
     Usuario: {
         salario(usuario){
             return usuario.salario_real
@@ -47,9 +60,16 @@ const resolvers = {
                 salario_real: 1345.67,
                 vip: true
             }
+        },
+        produtoEmDestaque() {
+            return {
+                nome: "Larissa",
+                preco: 100.00,
+                desconto: 50.00,
+            }  
         }
 
-    }
+    },
 }
 const server = new ApolloServer({
     typeDefs,
